f313b4cc6a47f22ed2a2db53e7f8139f014e2f5b
Vladimír Hanhur <aqgz@email.cz>
2025-08-16 22:46:00 +0200

    update

--- app.js
+++ app.js
@@ -1,23 +1,76 @@
 const fs = require('fs');
 
-fs.readFile('./text.txt', 'utf-8', (error, data) => {
-    fs.mkdirSync('./files', () => {});
-
-    fs.writeFileSync('./files/text2.txt', `${data} New text!`, (error) => {
-        error ? console.log(error) : null;
-    });  
+fs.readdir('./test', (error, files) => {
+    files.forEach(file => {
+        console.log('name: ' + file);
+    });
 });
 
-setTimeout(() => {
-    if(fs.existsSync('./files/text2.txt'))
-    {
-        fs.unlink('./files/text2.txt', () => {});
-    }
-}, 4000);
+// fs.rmdir("test", function(error)
+// {
+//     if(error)
+//     {
+//         console.log("error");
+//     }
+//     else 
+//     {
+//         console.log('delete');
+//     }
+// });
 
-setTimeout(() => {
-    if(fs.existsSync('./files'))
-    {
-        fs.rmdir('./files', () => {});
-    }
-}, 6000);
+// fs.mkdir('test', function(error)
+// {
+//     if(error && error == "EEXIST")
+//     {
+//         console.log(error);
+//     }
+//     else 
+//     {
+//         console.log('folder create'); 
+//     }
+// });
+
+// let arr;
+
+// arr = fs.readFileSync('./text.txt');
+// arr = arr.toString();
+// console.log(arr);
+
+
+// fs.readFile('./text.txt', function(err, data)
+// {
+//     if(err) throw err;
+//     arr = data.toString();
+//     arr = arr.split("\n");
+//     console.log(arr);
+// });
+
+// fs.readFile('./test.json', function(err, data)
+// {
+//     if(err) throw err;
+//     let arr = data.toString();
+//     arr = JSON.parse(arr);
+//     console.log(arr.name);
+// });
+
+// fs.readFile('./text.txt', 'utf-8', (error, data) => {
+//     fs.mkdirSync('./files', () => {});
+
+//     fs.writeFileSync('./files/text2.txt', `${data} New text!`, (error) => {
+//         error ? console.log(error) : null;
+//     });  
+// });
+
+// setTimeout(() => {
+//     if(fs.existsSync('./files/text2.txt'))
+//     {
+//         fs.unlink('./files/text2.txt', () => {});
+//     }
+// }, 4000);
+
+// setTimeout(() => {
+//     if(fs.existsSync('./files'))
+//     {
+//         fs.rmdir('./files', () => {});
+//     }
+// }, 6000);
