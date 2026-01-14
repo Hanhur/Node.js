29fd8f9fc7a54815b874c226bac4245b87baf889
Vladimír Hanhur <aqgz@email.cz>
2026-01-14 17:26:26 +0100

    update

--- NodeExpress/index.js
+++ NodeExpress/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const homeRoutes = require("./routes/home");
+const cardRoutes = require("./routes/card");
 const addRoutes = require("./routes/add");
 const coursesRoutes = require("./routes/courses");
+const path = require("path");
 
 const app = express();
 
@@ -15,12 +17,13 @@ app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "views");
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: true}));
 
 app.use("/", homeRoutes);
 app.use("/add", addRoutes);
 app.use("/courses", coursesRoutes);
+app.use("/card", cardRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
